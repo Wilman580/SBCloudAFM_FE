0e5991a5106d2c17744da6ca068a375488f1ee59
SONY-ROYAL <wgmatangog@utn.edu.ec>
2019-07-24 13:42:22 -0500

    Clase productos y creación de lista

---
+++ SBCloudAFM_FE/SBCloudAFM_FE/Entities/Productos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBCloudAFM_FE.Entities
+{
+    class Productos
+    {
+        public string nombrePro { get; set; }
+        public float precioPro { get; set; }
+
+        
+    }
+}
--- SBCloudAFM_FE/SBCloudAFM_FE/Form1.cs
+++ SBCloudAFM_FE/SBCloudAFM_FE/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SBCloudAFM_FE.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,31 @@ namespace SBCloudAFM_FE
 {
     public partial class Form1 : Form
     {
+        List<string> listaProductosString;
+        List<Productos> listaProductos;
+
         public Form1()
         {
             InitializeComponent();
+            creadorProducto();
+
+        }
+
+        public void creadorProducto()
+        {
+            listaProductosString = new List<string>();
+            listaProductosString.Add("GalletasRicas-0,50");
+            listaProductosString.Add("GalletasSalticas-0,60");
+            foreach (string producto in listaProductosString)
+            {
+                string[] vecpro = producto.Split('-');
+                string nombre = (string)vecpro.GetValue(0);
+                float precio = (float)vecpro.GetValue(1);
+                Productos p = new Productos();
+                p.nombrePro = nombre;
+                p.precioPro = precio;
+                listaProductos.Add(p);
+            }
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
